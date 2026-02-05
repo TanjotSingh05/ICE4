1e20e25999550d62aeadbe3ab384c284f305ad78
tanjotsingh <tanjotsingh@localhost>
2026-02-05 18:18:44 -0500

    First commit

---
+++ src/main/java/com/mycompany/projecttanjotsingh/ProjectTanjotSingh.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecttanjotsingh;
+
+/**
+ *
+ * @author tanjotsingh
+ */
+public class ProjectTanjotSingh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+}
